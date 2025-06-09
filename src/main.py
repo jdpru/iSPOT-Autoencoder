42561536b5cf2aa88f38109008e0f992be15aa8f
Janelle Cheung <janellecheung@Janelles-MacBook-Air.local>
2025-06-09 06:00:03 -0700

    Created basic 5 experiments: clinical +- unsup/semisup vanilla autoencoder eeg features

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import h5py
 import numpy as np
 import pandas as pd
 import torch
@@ -17,47 +16,15 @@ def main():
     data = load_train_test_data()
     train_eeg_dataloader, test_eeg_dataloader = create_eeg_dataloaders(data)
 
-    train_clinical_data = data['train']['clinical_data']
-    test_clinical_data = data['test']['clinical_data']
-    train_y = data['train']['response']
-    test_y = data['test']['response']
+    # Run all experiments
+    results = run_all_experiments(data, train_eeg_dataloader, test_eeg_dataloader)
 
-    # TO-DO: CREATE EXPERIMENT FUNCTIONS FOR EACH OF THESE
-
-    model1 = BaselineAutoencoder()
-    train_unsupervised_autoencoder(model1, train_eeg_dataloader)
-    train_eeg_latent_features_unsup = extract_latent_features(train_eeg_dataloader)
-    test_eeg_latent_features_unsup = extract_latent_features(test_eeg_dataloader)
-
-    model2 = SemiSupervisedAutoencoder()
-    train_semisupervised_autoencoder(model2, train_eeg_dataloader)
-    train_eeg_latent_features_semisup = extract_latent_features(train_eeg_dataloader)
-    test_eeg_latent_features_semisup = extract_latent_features(test_eeg_dataloader)
-    
-    # Train and evaluate all features with logistic regression
-    train_and_evaluate_logreg(
-        train_clinical_data,
-        test_clinical_data,
-        train_y, test_y
-    )
-
-    train_and_evaluate_logreg(
-        train_eeg_latent_features_unsup, 
-        test_eeg_latent_features_unsup, 
-        train_y, test_y
-    )
-
-    train_and_evaluate_logreg(
-        train_eeg_latent_features_semisup, 
-        test_eeg_latent_features_semisup, 
-        train_y, test_y
-    )
-
-    train_and_evaluate_logreg(
-        np.hstack((train_eeg_latent_features_unsup, train_clinical_data)), 
-        np.hstack((test_eeg_latent_features_semisup, test_clinical_data)), 
-        train_y, test_y
-    )
+    # Analyze results
+    print("\n" + "=" * 60)
+    print("EXPERIMENT SUMMARY")
+    print("=" * 60)
+    for experiment_name in results.keys():
+        print(f"✓ {experiment_name.replace('_', ' ').title()} - Complete")
 
 if __name__ == '__main__':
     setup()
